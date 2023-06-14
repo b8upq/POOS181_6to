a6c349a375750f3e25f91134b960606c25140dca
Jesus Almaguer <111038018+b8upq@users.noreply.github.com>
2023-06-14 17:10:23 -0600

    Conexion de bd, creacion de rutas guardar y eliminar

--- Practica 5/app.py
+++ Practica 5/app.py
@@ -1,10 +1,30 @@
+#Importacion del framework a utilizar
 from flask import Flask
+from flask_mysqldb import MySQL
 
+#Inicializacion de la aplicacion 
 app = Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='dbflask'
+mysql=MySQL(app)
 
-@app.route('/')
+#Inicialización de las ruta http://127.0.0.1:5000
+@app.route('/') #Se le conoce como ruta index (por ser la unica ruta con esa diagonal)
 def index():
     return "Hola Mundo FLASK"
 
+@app.route('/guardar')
+def guardar():
+    return "Se guardo en la BD"
+
+@app.route('/eliminar')
+def eliminar():
+    return "Se elimino en la BD"
+
+#Ejecucion del servidor en el puerto 5000
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
+    
+    
