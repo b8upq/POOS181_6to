eb0c709cc591d70cc52fbbef4c1b5713b11a2341
Jesus Almaguer <111038018+b8upq@users.noreply.github.com>
2023-06-28 16:14:14 -0600

    Correciones de commit, correciones de codigo, implementacion de nuevas funciones y metodos en cada uno de los proyectos

--- Practica 5/app.py
+++ Practica 5/app.py
@@ -1,5 +1,5 @@
 #Importacion del framework a utilizar
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 #Inicializacion de la aplicacion 
@@ -8,16 +8,28 @@ app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='dbflask'
+app.config_key='mysecretkey'
 mysql=MySQL(app)
 
 #Inicialización de las ruta http://127.0.0.1:5000
 @app.route('/') #Se le conoce como ruta index (por ser la unica ruta con esa diagonal)
 def index():
-    return "Hola Mundo FLASK"
+    #return "Hola Mundo FLASK"
+    return render_template('index.html')
 
-@app.route('/guardar')
+@app.route('/guardar', methods=['POST'])
 def guardar():
-    return "Se guardo en la BD"
+    if request.method == 'POST':
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        print (titulo, artista, anio)
+        
+        
+    """ return "se guardó en la BD " """
+    flash('El album fue agregado correctamente.')
+    return redirect(url_for('index')) 
+
 
 @app.route('/eliminar')
 def eliminar():
@@ -25,6 +37,7 @@ def eliminar():
 
 #Ejecucion del servidor en el puerto 5000
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
+    #app.run(port=5000, debug=True)
     
     
